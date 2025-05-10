8fc65723c06ee074a5ce5d9a95a72af1a2c60ae3
Ergyon <benoitdoudard56@gmail.com>
2025-05-10 17:09:53 +0200

    updated sliders

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ const sliders = [
             "txt":"Une petite descriptions plus ou moins longue selon mon inspi du moment"
         },
         {
-            "img":"img/snare2.jpg",
+            "img":"img/drumsover.jpg",
             "txt":"Une deuxieme petite descriptions peut-etre un peu moins longue selon mon inspi du moment"
         },
         {
-            "img":"img/drums.jpg",
+            "img":"img/ride.jpg",
             "txt":"Une troisieme petite descriptions beaucoup plus longue car je suis tres inspire regardez le nombre de mots que je vient d'ecrire c'est pas mal en vrai"
         }  
     ],
@@ -40,7 +40,7 @@ const sliders = [
            "txt":"Une deuxieme petite descriptions peut-etre un peu moins longue selon mon inspi du moment"
        },
        {
-           "img":"img/piano-keys.jpg",
+           "img":"img/piano-mechanics-zoom.jpg",
            "txt":"Une troisieme petite descriptions beaucoup plus longue car je suis tres inspire regardez le nombre de mots que je vient d'ecrire c'est pas mal en vrai"
        }
     ]    
