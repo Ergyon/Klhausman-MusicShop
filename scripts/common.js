4a8579a9ce623a728b80b7fd742d470b1c4d74fd
Ergyon <benoitdoudard56@gmail.com>
2025-07-01 04:44:17 +0200

    new pages and filter function

--- scripts/common.js
+++ scripts/common.js
@@ -35,6 +35,131 @@ window.addEventListener("DOMContentLoaded", () => {
         sideModalShop.classList.remove("active")
         overlay.classList.add("hidden")
     })
-
-
 }) 
+
+// Afficher image plein ecran
+function showFullScreenImage(imgSrc, altText) {
+    const existingOverlay = document.querySelector(".overlay-fullscreen")
+    if (existingOverlay) 
+        return
+
+    const overlay = document.createElement("div")
+    overlay.classList.add("overlay-fullscreen")
+
+    const fullImg = document.createElement("img")
+    fullImg.classList.add("img-fullscreen")
+    fullImg.src = imgSrc
+    fullImg.alt = altText
+
+    overlay.appendChild(fullImg)
+    document.body.appendChild(overlay)
+
+    overlay.addEventListener("click", () => {
+        overlay.remove()
+    })  
+}
+
+// Creer liste produit dans le panier
+let cart = []
+const cartSummary = document.querySelector(".cart__summary")
+cartSummary.style.display = "none"
+
+function addToCart(product, addCartBtn) {
+    const cartItemsList = document.querySelector(".cart--items")
+      
+    const cartItem = document.createElement("li")
+    cartItem.classList.add("cart--items__item")
+
+    const imgItemShop = document.createElement("img")
+    imgItemShop.classList.add("cart--items__item__img")
+    imgItemShop.src = product.img
+    imgItemShop.alt = product.name
+
+    const itemsInfos = document.createElement("div")
+    itemsInfos.classList.add("items__details")
+
+    const nameItemshop = document.createElement("span")
+    nameItemshop.classList.add("items__details__name")
+    nameItemshop.textContent = product.name
+
+    const priceItemShop = document.createElement("span")
+    priceItemShop.classList.add("items__details__price")
+    priceItemShop.textContent = product.price.toLocaleString("fr-FR") + " $"
+
+    const removeItemShop = document.createElement("button")
+    removeItemShop.classList.add("items__details__remove")
+    removeItemShop.textContent = "Retirer"
+
+    const lineSeparator = document.createElement("div")
+    lineSeparator.classList.add("items__details--separator")
+
+    itemsInfos.appendChild(nameItemshop)
+    itemsInfos.appendChild(priceItemShop)
+    itemsInfos.appendChild(removeItemShop)
+    itemsInfos.appendChild(lineSeparator)
+    
+    cartItem.appendChild(imgItemShop)
+    cartItem.appendChild(itemsInfos)
+    cartItemsList.appendChild(cartItem)
+
+    // push le porduit dans le panier
+    cart.push(product)
+    cartSummary.style.display = "flex"
+    updateTotal()
+
+    // produit checked
+    addCartBtn.innerHTML ="Ajouté au panier"
+    addCartBtn.classList.add("product__btn--addCart--checked")
+
+    // Retirer du panier
+    removeItemShop.addEventListener("click", () => {
+        cartItem.remove()
+        addCartBtn.classList.remove("product__btn--addCart--checked")
+        addCartBtn.innerHTML = "Sélectionner ce produit"
+        
+        const index = cart.indexOf(product)
+        if (index !== -1) {
+            cart.splice(index, 1)
+        }
+        
+        updateTotal()
+        localStorage.setItem("cart", JSON.stringify(cart))
+    })
+    
+    // Tout retirer du panier
+    removeAll.addEventListener("click", () => {
+        cartItemsList.innerHTML = ""
+        cart = []
+        cartSummary.style.display = "none"
+
+        const allBtnsChecked = document.querySelectorAll(".product__btn--addCart")
+        allBtnsChecked.forEach((btn) => {
+            btn.classList.remove("product__btn--addCart--checked")
+            btn.textContent = "Sélectionner ce produit"
+        })
+
+        updateTotal()
+        localStorage.removeItem("cart")
+    })
+
+    localStorage.setItem("cart", JSON.stringify(cart))
+}
+
+// Calculer total panier
+function updateTotal() {
+    const tax = document.getElementById("tax")
+    const totalContent = document.getElementById("total")
+    let total = 0
+    
+    cart.forEach(product => {
+        total += product.price
+    })
+    
+    tax.innerHTML = "TVA : 2,4%"
+    totalContent.innerHTML = "Total : " + total.toLocaleString("fr-FR") + " $"
+    
+    if (cart.length === 0) {
+        cartSummary.classList.add("hidden")
+    }
+}
+
---
+++ scripts/filter.js
@@ -0,0 +1,124 @@
+// Recuparations de tous les produits + categorie de filtre de la page
+async function getAllProducts() {
+    try {
+        const response = await fetch("data/products-data.json")
+        if (!response.ok) {
+            throw new Error("Impossible de charger les donnees")
+        }
+
+        const allProducts = response.json()
+        return allProducts
+
+    } catch(error) {
+        console.log("Impossible de collecter les donnees :", error)
+        return null
+    }
+}
+
+function getPageType() {
+    const url = window.location.href
+    if (url.includes("new")) return "new"
+    if (url.includes("popular")) return "popular"
+    if (url.includes("uniques")) return "uniques"
+    return null
+}
+
+// Filtre nouveaux, populaires, uniques
+function filterNew(allProducts) {
+    return Object.values(allProducts).flatMap(products => products).filter(product => product.new === true)
+}
+
+function filterPopular(allProducts) {
+    return Object.values(allProducts).flatMap(products => products).filter(product => product.popular === true)
+}
+
+function filterUniques(allProducts) {
+    return Object.values(allProducts).flatMap(products => products).filter(product => product.uniques === true)
+}
+
+function displayFiltered(products) {
+    const container = document.querySelector(".newGallery")
+    if (!container || !products.length) {
+        container.innerHTML = "Aucun produit à afficher"
+        return
+    }
+
+    products.forEach(product => {
+        const card = document.createElement("card")
+        card.classList.add("newGallery__card")
+
+        const cardImg = document.createElement("img")
+        cardImg.classList.add("newGallery__card__img")
+        cardImg.src = product.img
+        cardImg.alt = product.name
+
+        const cardName = document.createElement("h4")
+        cardName.classList.add("newGallery__card__name")
+        cardName.textContent = product.name
+
+        const cardPrice = document.createElement("span")
+        cardPrice.classList.add("newGallery__card__price")
+        cardPrice.textContent = product.price
+
+        const addBtn = document.createElement("button")
+        addBtn.classList.add("newGallery__card__addBtn")
+        addBtn.textContent = "Ajouter au panier"
+
+        card.appendChild(cardImg)
+        card.appendChild(cardName)
+        card.appendChild(cardPrice)
+        card.appendChild(addBtn)
+        container.appendChild(card)
+    })
+}
+
+// Animation clavier
+const subtitleLetters = "Fraîchement sortis de l’atelier, nos nouveaux instruments n’attendent que de trouver leur voix."
+let index = 0
+const headlineNewSub = document.getElementById("headlineNewSub")
+
+function typedLetters() {
+    if(index < subtitleLetters.length) {
+        headlineNewSub.textContent += subtitleLetters.charAt(index)
+        index++
+        setTimeout(typedLetters, 35)
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(typedLetters, 1100)
+})
+
+// Initialisation
+async function initialize() {
+    const data = await getAllProducts()
+
+    if (!data) {
+        console.error("Produits manquants")
+        return
+    }
+
+    const pageType = getPageType()
+    let filteredProducts = []
+
+    switch (pageType) {
+        case "new":
+            filteredProducts = filterNew(data)
+            break
+        case "popular":
+            filteredProducts = filterPopular(data)
+            break
+        case "uniques":
+            filteredProducts = filterUniques(data)
+            break
+        default:
+            console.error("Aucune page existante")
+            return
+    }
+
+    displayFiltered(filteredProducts)
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    initialize()
+})
